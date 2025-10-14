3cce674a847277be0185b3a3772020e63b4dcc5a
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-10-14 17:56:27 +0900

    build : AreaBasedList2 테이블 제거 - AreaBasedSyncList2, DetailCommon2 : 테이블에 대한 기본적인 Dto 생성 - 나머지 Dto : Dto 클래스만 생성, 기본적인 정보 미완

---
+++ src/main/java/rootLab/model/dto/api/DetailInfo2_12Dto.java
@@ -0,0 +1,17 @@
+package rootLab.model.dto.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <p> Api_Origin DB의 DetailInfo2_12 테이블에 대한 클래스.
+ * <p> DetailInfo2_12 테이블에 대한 접근을 지원합니다.
+ * @author AhnJH
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetailInfo2_12Dto {
+} // class end
