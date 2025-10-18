68d6b7f71e68a6d917316bf412617b32c6414862
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-10-18 15:21:55 +0900

    feat : 파일 삭제 기능 구현 - FileUtil.java : 파일 삭제 기능 구현 | 파일 업로드 경로에 대한 논의 필요

--- src/main/java/rootLab/util/file/FileUtil.java
+++ src/main/java/rootLab/util/file/FileUtil.java
@@ -16,7 +16,7 @@ public class FileUtil {
     private String uploadPath = baseDir + "/build/resources/main/img/";
 
     /**
-     * 업로드된 파일을 받아서, 파일을 업로드하는 기능
+     * [업로드 파일, 카테고리 타입]을 받아서, 파일을 업로드한다.
      *
      * @param multipartFile 업로드된 파일
      * @param imgCategory   카테고리 타입(마커, 푸시, 파비콘, 로고 등)
@@ -47,5 +47,29 @@ public class FileUtil {
         } // try-catch end
         // 8. 최종적으로 UUID가 포함된 파일명 반환
         return fileName;
+    } // func end
+
+    /**
+     * [카테고리 타입, 삭제할 파일명]을 받아서, 해당하는 파일을 삭제한다.
+     *
+     * @param imgCategory 카테고리 타입(마커, 푸시, 파비콘, 로고 등)
+     * @param fileName 삭제할 파일명
+     * @return 파일삭제 성공여부
+     * @author AhnJH
+     */
+    public boolean deleteFile(String imgCategory, String fileName){
+        // 1. 삭제할 파일명과 경로 생성
+        String deleteFilePath = uploadPath + imgCategory + "/" + fileName;
+        // 2. 경로에 대한 File 객체 생성
+        File deleteFile = new File(deleteFilePath);
+        // 3. 삭제 진행
+        if (deleteFile.exists()){
+            // 3-1. 경로에 파일이 존재하면, 삭제 및 true 반환
+            deleteFile.delete();
+            return true;
+        } else {
+            // 3-2. 존재하지 않으면, false 반환
+            return false;
+        } // if end
     } // func end
 } // class end
