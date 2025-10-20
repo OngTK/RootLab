2435e584cc2a98676249ca2f4daf7b6869f3dba5
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-10-20 09:26:48 +0900

    feat : detailIntro2_15 추가에 따른 SQL 추가 - Api_Origin.sql : detailIntro2_15 테이블 생성 및 INSERT SQL 추가

--- src/main/resources/sql/정은주_Api_Origin.sql
+++ src/main/resources/sql/정은주_Api_Origin.sql
@@ -5,7 +5,7 @@
     -> 경로 작성 시, \ -> \\로 변경 요망
 3. 경로를 모두 변경했다면, 전체 실행을 함으로써 모든 CSV 데이터를 DB에 추가
 
-** 기본적으로 varchar(255)로 진행하였으나, 크기가 큰 데이터가 있어 일부는 text로 진행. -> 추후 자동화할 때는 어떻게 진행할 지 논의 필요
+** 기본적으로 VARCHAR(255)로 진행하였으나, 크기가 큰 데이터가 있어 일부는 TEXT로 진행. -> 추후 자동화할 때는 어떻게 진행할 지 논의 필요
 */
 DROP DATABASE IF EXISTS tour_api_origin;
 CREATE DATABASE tour_api_origin;
@@ -15,10 +15,11 @@ SET GLOBAL local_infile=TRUE;	# 로컬 파일을 읽을 수 있도록 설정
 
 CREATE TABLE areaBasedSyncList2(addr1 VARCHAR(255),addr2 VARCHAR(255),areacode VARCHAR(255),cat1 VARCHAR(255),cat2 VARCHAR(255),cat3 VARCHAR(255),contentid VARCHAR(255),contenttypeid VARCHAR(255),createdtime VARCHAR(255),firstimage VARCHAR(255),firstimage2 VARCHAR(255),cpyrhtDivCd VARCHAR(255),mapx VARCHAR(255),mapy VARCHAR(255),mlevel VARCHAR(255),modifiedtime VARCHAR(255),sigungucode VARCHAR(255),tel TEXT,title VARCHAR(255),zipcode VARCHAR(255),showflag VARCHAR(255),lDongRegnCd VARCHAR(255),lDongSignguCd VARCHAR(255),lclsSystm1 VARCHAR(255),lclsSystm2 VARCHAR(255),lclsSystm3 VARCHAR(255));
 CREATE TABLE detailCommon2(contentid VARCHAR(255),contenttypeid VARCHAR(255),title VARCHAR(255),createdtime VARCHAR(255),modifiedtime VARCHAR(255),tel VARCHAR(255),telname VARCHAR(255),homepage TEXT,firstimage VARCHAR(255),firstimage2 VARCHAR(255),cpyrhtDivCd VARCHAR(255),areacode VARCHAR(255),sigungucode VARCHAR(255),lDongRegnCd VARCHAR(255),lDongSignguCd VARCHAR(255),lclsSystm1 VARCHAR(255),lclsSystm2 VARCHAR(255),lclsSystm3 VARCHAR(255),cat1 VARCHAR(255),cat2 VARCHAR(255),cat3 VARCHAR(255),addr1 VARCHAR(255),addr2 VARCHAR(255),zipcode VARCHAR(255),mapx VARCHAR(255),mapy VARCHAR(255),mlevel VARCHAR(255),overview TEXT);
-CREATE TABLE detailImage2(contentid VARCHAR(255), originimgurl VARCHAR(255), imgname VARCHAR(255), smallimageurl VARCHAR(255), cpyrhtDivCd VARCHAR(255), serialnum VARCHAR(255));
+CREATE TABLE detailImage2(contentid VARCHAR(255),originimgurl VARCHAR(255),imgname VARCHAR(255),smallimageurl VARCHAR(255),cpyrhtDivCd VARCHAR(255),serialnum VARCHAR(255));
 CREATE TABLE detailInfo2_12(contentid VARCHAR(255),contenttypeid VARCHAR(255),serialnum VARCHAR(255),infoname VARCHAR(255),infoTEXT VARCHAR(255),fldgubun VARCHAR(255));
 CREATE TABLE detailInfo2_39(contentid VARCHAR(255),contenttypeid VARCHAR(255),serialnum VARCHAR(255),infoname VARCHAR(255),infoTEXT VARCHAR(255),fldgubun VARCHAR(255));
 CREATE TABLE detailIntro2_12(contentid VARCHAR(255),contenttypeid VARCHAR(255),heritage1 VARCHAR(255),heritage2 VARCHAR(255),heritage3 VARCHAR(255),infocenter VARCHAR(255),opendate VARCHAR(255),restdate VARCHAR(255),expguide VARCHAR(255),expagerange VARCHAR(255),accomcount VARCHAR(255),useseason VARCHAR(255),usetime VARCHAR(255),parking VARCHAR(255),chkbabycarriage VARCHAR(255),chkpet VARCHAR(255),chkcreditcard VARCHAR(255));
+CREATE TABLE detailIntro2_15(contentid VARCHAR(255),contenttypeid VARCHAR(255),agelimit VARCHAR(255),bookingplace VARCHAR(255),discountinfofestival VARCHAR(255),eventenddate VARCHAR(255),eventhomepage VARCHAR(255),eventplace VARCHAR(255),eventstartdate VARCHAR(255),festivalgrade VARCHAR(255),festivaltype VARCHAR(255),placeinfo VARCHAR(255),playtime VARCHAR(255),program VARCHAR(255),progresstype VARCHAR(255),spendtimefestival VARCHAR(255),sponsor1 VARCHAR(255),sponsor1tel VARCHAR(255),sponsor2 VARCHAR(255),sponsor2tel VARCHAR(255),subevent VARCHAR(255),usetimefestival VARCHAR(255));
 CREATE TABLE detailIntro2_39(contentid VARCHAR(255),contenttypeid VARCHAR(255),seat VARCHAR(255),kidsfacility VARCHAR(255),firstmenu VARCHAR(255),treatmenu TEXT,smoking VARCHAR(255),packing VARCHAR(255),infocenterfood VARCHAR(255),scalefood VARCHAR(255),parkingfood VARCHAR(255),opendatefood VARCHAR(255),opentimefood VARCHAR(255),restdatefood VARCHAR(255),discountinfofood VARCHAR(255),chkcreditcardfood VARCHAR(255),reservationfood VARCHAR(255),lcnsno VARCHAR(255));
 CREATE TABLE detailPetTour2(contentid VARCHAR(255),relaAcdntRiskMtr TEXT,acmpyTypeCd VARCHAR(255),relaPosesFclty TEXT,relaFrnshPrdlst VARCHAR(255),etcAcmpyInfo TEXT,relaPurcPrdlst VARCHAR(255),acmpyPsblCpam TEXT,relaRntlPrdlst VARCHAR(255),acmpyNeedMtr VARCHAR(255));
 CREATE TABLE lclsSystmCode2(lclsSystm1Cd VARCHAR(255),lclsSystm1Nm VARCHAR(255),lclsSystm2Cd VARCHAR(255),lclsSystm2Nm VARCHAR(255),lclsSystm3Cd VARCHAR(255),lclsSystm3Nm VARCHAR(255),rnum VARCHAR(255));
@@ -53,6 +54,11 @@ LINES TERMINATED BY "\n"
 IGNORE 1 ROWS;
 LOAD DATA LOCAL INFILE "C:\\Users\\tj-bu-702-06\\Desktop\\RootLab\\src\\main\\resources\\csv\\251012\\detailIntro2_12.csv"
 INTO TABLE tour_api_origin.detailIntro2_12
+FIELDS TERMINATED BY ","
+LINES TERMINATED BY "\n"
+IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE "C:\\Users\\tj-bu-702-06\\Desktop\\RootLab\\src\\main\\resources\\csv\\251012\\detailIntro2_15.csv"
+INTO TABLE tour_api_origin.detailIntro2_15
 FIELDS TERMINATED BY ","
 LINES TERMINATED BY "\n"
 IGNORE 1 ROWS;
