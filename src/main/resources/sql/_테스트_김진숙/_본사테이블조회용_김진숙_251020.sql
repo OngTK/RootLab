a111110a8c391deb5b351ca4eeb212982dfd8c17
root-kjs <root.kjs82@gmail.com>
2025-10-20 16:18:35 +0900

    chore(sql): 김진숙 테스트 sql 폴더 정리

---
+++ src/main/resources/sql/_테스트_김진숙/_본사테이블조회용_김진숙_251020.sql
@@ -0,0 +1,16 @@
+select * from pushPopup;			# 푸시알림/팝업(*자체 테이블)
+select * from manager;			# 관리자정보(*자체 테이블)
+select * from siteInfo;			# 사이트정보(*자체 테이블)
+select * from placeInfoRepeat;   # 플레이스 반복정보(*api)
+select * from restaurantIntro;	# 음식점(39) 상세정보(*api)areabasedsynclist2
+select * from festivalIntro; 	# 축제공연행사(15) 상세정보(*api)
+select * from tourIntro;			# 관광지(12) 상세정보(*api)
+select * from detailPetTour;		# 반려동물 동반여행 상세정보(*api)
+select * from placeImageDetail;	# 플레이스 상세이미지(*api)
+select * from markersGPS;		# 지도마커GPS(*api)
+select * from placeInfo;			# 플레이스 기본정보(*api)
+select * from contentType;		# 콘텐츠타입(*api: 총8개 타입) 1.관광지(12) 2.문화시설(14) 3.행사공연축제(15) 4.여행코스(25) 5.레포츠(28) 6.숙박(32) 7.쇼핑(38) 8.음식점(39)
+select * from categoryCode;		# 분류체계코드(*api)
+select * from ldongCode;			# 법정동코드(*api)
+
+USE tour_api_origin;
