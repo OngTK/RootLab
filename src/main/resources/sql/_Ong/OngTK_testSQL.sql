8f9fe9e11704cbfe80139b46eb3724ffb70ea767
OngTK <dhdxorud@gmail.com>
2025-10-21 18:01:27 +0900

    feat(placeInfo) : search
    plceinfo 검색 기능 구현중

--- src/main/resources/sql/_Ong/OngTK_testSQL.sql
+++ src/main/resources/sql/_Ong/OngTK_testSQL.sql
@@ -40,16 +40,7 @@ select * from siteinfo;
 select * from tourintro;
 select * from push;
 
--- ---------------------------------------------------------
--- PlaceInfoRepeat : 반복정보 관련
--- ---------------------------------------------------------
 
-SELECT * FROM PLACEINFOREPEAT;
-insert into placeInfoRepeat( pNo, infoName , infoText, serialnum ) values (125183, "화장실","비밀번호 뭐게요", 0);
-select * from placeinforepeat where pNo = "125183";
-update placeinforepeat set infoname = "화장실수정11", infotext = "비밀번호 수정" where pirNo = "2";
-alter table placeinforepeat change infoname infoname varchar(30); 
-desc placeinforepeat;
 
 -- ---------------------------------------------------------
 -- PlaceInfo : 플레이스 정보
@@ -58,55 +49,7 @@ desc placeinforepeat;
 
 select * from placeInfo;
 
-insert into placeInfo(ctNo, ldNo, ccNO, title, showflag, firstImage, firstImage2, addr1, zipcode, homepage, tel)
-values (1, 1, 1, "제목",1,"임시이미지1.jpg","임시이미지2.jpg", "경기도 부천시","00000","https://~~~","032-000-00000" );
-
-INSERT INTO k_tour_headquarter.placeinfo (ctNo, ldNo, ccNo, isEditable, contentid, title, showflag, firstimage, firstimage2, addr1, addr2, zipcode, homepage, tel, telname, overview, createdAt, updatedAt)
-            SELECT
-                /* FK: contentType.ctNo */
-                ( SELECT ct.ctNo
-                  FROM k_tour_headquarter.contentType ct
-                  WHERE ct.contenttypeID = TRIM(al.contenttypeid)
-                    LIMIT 1 ) AS ctNo,
-
-                /* FK: ldongCode.ldNo (법정동: lDongRegnCd + lDongSignguCd) */
-                ( SELECT ld.ldNo
-                FROM k_tour_headquarter.ldongCode ld
-                WHERE ld.lDongRegnCd = TRIM(al.lDongRegnCd)
-                AND ld.lDongSignguCd= TRIM(al.lDongSignguCd)
-                LIMIT 1 ) AS ldNo,
-
-                /* FK: categoryCode.ccNo (al.lclsSystm3 기준 매칭) */
-                ( SELECT cc.ccNo
-                FROM k_tour_headquarter.categoryCode cc
-                WHERE cc.lclsSystm3Cd =
-                UPPER(TRIM(REPLACE(REPLACE(REPLACE(REPLACE(
-                al.lclsSystm3, CHAR(13), ''), CHAR(10), ''), CHAR(9), ''), ' ', '')))
-                LIMIT 1 ) AS ccNo,
-
-                TRUE AS isEditable,
-                CAST(al.contentid AS UNSIGNED) AS contentid,
-                LEFT(TRIM(al.title), 50) AS title,
-                al.showflag AS showflag,
-                NULLIF(TRIM(al.firstimage), '') AS firstimage,
-                NULLIF(TRIM(al.firstimage2), '') AS firstimage2,
-                COALESCE(NULLIF(TRIM(al.addr1), ''), '-') AS addr1,
-                NULLIF(TRIM(al.addr2), '') AS addr2,
-                NULLIF(TRIM(al.zipcode), '') AS zipcode,
-
-                /* 부가정보: dc가 있으면 채움, 없으면 NULL */
-                NULLIF(TRIM(dc.homepage), '') AS homepage,
-                LEFT(NULLIF(TRIM(al.tel), ''), 10) AS tel,
-                NULLIF(TRIM(dc.telname), '') AS telname,
-                NULLIF(dc.overview, '') AS overview,
-
-                /* [] createdAt, updatedAt을 DATETIME 형식으로 변환 */
-                STR_TO_DATE(al.createdtime, '%Y%m%d%H%i%s') AS createdAt,
-                STR_TO_DATE(al.modifiedtime, '%Y%m%d%H%i%s') AS updatedAt
-
-            FROM tour_api_origin.areabasedsynclist2 al
-            LEFT JOIN tour_api_origin.detailcommon2 dc
-            ON CAST(TRIM(al.contentid) AS UNSIGNED) = CAST(TRIM(dc.contentid) AS UNSIGNED);
+select count(*) from placeinfo;
             
 -- ---------------------------------------------------------
 -- TourIntro
@@ -130,6 +73,17 @@ select * from RestaurantIntro where pno = 51385;
 
 
 -- ---------------------------------------------------------
+-- PlaceInfoRepeat : 반복정보 관련
+-- ---------------------------------------------------------
+
+SELECT * FROM PLACEINFOREPEAT;
+insert into placeInfoRepeat( pNo, infoName , infoText, serialnum ) values (125183, "화장실","비밀번호 뭐게요", 0);
+select * from placeinforepeat where pNo = "125183";
+update placeinforepeat set infoname = "화장실수정11", infotext = "비밀번호 수정" where pirNo = "2";
+alter table placeinforepeat change infoname infoname varchar(30); 
+desc placeinforepeat;
+
+-- ---------------------------------------------------------
 -- placeimagedetail
 -- 플레이스 사진 정보
 -- ---------------------------------------------------------
@@ -140,3 +94,4 @@ select * from placeimagedetail where pno = 6881;
 -- MarkerGPS
 -- 마커 이미지 관리 table
 -- ---------------------------------------------------------
+select * from markersgps where pno = 6881;
