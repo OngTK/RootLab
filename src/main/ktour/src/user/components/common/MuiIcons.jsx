dfacadfc0e7136d9fd2a4fb5de5242b31152e75c
root-kjs <root.kjs82@gmail.com>
2025-10-22 18:10:55 +0900

    feat(style): MUI Icons (SVG) 설치 및 테스트 - 구글폰트보다 효율적인 MUI Icons (SVG) 사용하기로 함.

---
+++ src/main/ktour/src/user/components/common/MuiIcons.jsx
@@ -0,0 +1,27 @@
+/**
+ * K-Tour > 사용자단 MUIIcons 컴포넌트
+ * https://mui.com/material-ui/material-icons/
+ *
+ * @author kimJS
+ * @since 2025.10.22
+ * @version 0.1.0
+ */
+
+import FavoriteTwoToneIcon from '@mui/icons-material/FavoriteTwoTone';
+import { Box } from '@mui/material';
+
+export default function TwoToneColorExample( props ) {
+  return (
+    <Box sx={{ display: 'flex', gap: 3, alignItems: 'center' }}>
+      {/* 기본 색상: 회색 + 흐릿한 회색 */}
+      <FavoriteTwoToneIcon sx={{ fontSize: 60 }} />
+      
+      {/* 색상 변경: 주황색 계열의 투톤으로 변경 */}
+      <FavoriteTwoToneIcon sx={{ color: 'orange', fontSize: 60 }} />
+      
+      {/* 색상 변경: 보라색 계열의 투톤으로 변경 */}
+      <FavoriteTwoToneIcon sx={{ color: '#9C27B0', fontSize: 60 }} />
+    </Box>
+  );
+}
+// 출력: 주황색 투톤 하트, 보라색 투톤 하트 아이콘
--- src/main/ktour/src/user/components/layout/LayoutUser.jsx
+++ src/main/ktour/src/user/components/layout/LayoutUser.jsx
@@ -5,17 +5,19 @@
  * @since 2025.10.19
  * @version 0.1.0
  */
-import Header from "./Header";              // 헤더 컴포넌트
-import AsideLnb from "./AsideLnb";          // 사이드 네비게이션 컴포넌트
-import PageTitle from "./PageTitle";        // 페이지 타이틀 컴포넌트
-import Footer from "./Footer";              // 푸터 컴포넌트
-import { Outlet } from "react-router-dom";  // 중첩된 라우트 컴포넌트를 보여주기 위한 컴포넌트, 공통 레이아웃을 유지하면서 콘텐츠 영역만 자식 페이지로 변경
+import Header from "./Header";                                 // 헤더 컴포넌트
+import AsideLnb from "./AsideLnb";                             // 사이드 네비게이션 컴포넌트
+import PageTitle from "./PageTitle";                           // 페이지 타이틀 컴포넌트
+import Footer from "./Footer";                                 // 푸터 컴포넌트
+import { Outlet } from "react-router-dom";                     // 중첩된 라우트 컴포넌트를 보여주기 위한 컴포넌트, 공통 레이아웃을 유지하면서 콘텐츠 영역만 자식 페이지로 변경
+import TwoToneColorExample from "@user/components/common/MuiIcons";       // MUI 아이콘 컴포넌트
 export default function LayoutUser() {
   return (
     <>
       <Header />
       <AsideLnb />
       <PageTitle />
+      <TwoToneColorExample />
       <main><Outlet /></main>
       <Footer />
     </>
