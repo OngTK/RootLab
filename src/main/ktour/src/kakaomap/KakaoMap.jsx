3b103c8f87f9b5bec8d5434071f3e428304fa797
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-10-22 18:08:25 +0900

    feat : 마커 구현 완료 및 클러스터러 미구현 - png : resources -> assets 폴더로 이동

--- src/main/ktour/src/kakaomap/KakaoMap.jsx
+++ src/main/ktour/src/kakaomap/KakaoMap.jsx
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import { Map } from "react-kakao-maps-sdk";
+import React, { useEffect, useState } from 'react';
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 import UseKakaoLoader from './UseKakaoLoader';
 import axios from "axios";
-
-export default function KakaoMap(props){
+import xxx from '../assets/contentTypeMarker/festival.png'
+export default function KakaoMap(props) {
     UseKakaoLoader();
     // =================== 렌더링 기준 사방좌표 구하기 ===================
     const [bounds, setBounds] = useState({
-        south : "0.0",
-        west : "0.0",
-        north : "0.0",
-        east : "0.0"
+        south: "0.0",
+        west: "0.0",
+        north: "0.0",
+        east: "0.0"
     }); // useState end
     // =================== 현재 위도경도 구하기 ===================
     const [initialMap, setInitialMap] = useState(null);
@@ -22,10 +22,10 @@ export default function KakaoMap(props){
         errMsg: null,
         isLoading: true
     }); // useState end
-// =================== useEffect - [initialMap] ===================
-    useEffect( () => {
+    // =================== useEffect - [initialMap] ===================
+    useEffect(() => {
         // geolocation을 통해 위도·경도를 얻을 수 있다면
-        if (navigator.geolocation){
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((location) => {
                 setCurrentLocation((prev) => ({
                     ...prev,
@@ -36,27 +36,27 @@ export default function KakaoMap(props){
                     isLoading: false
                 })) // setCurrentLocation end
             }, // location end
-            (error) => {
-                setCurrentLocation((prev) => ({
-                    ...prev,
-                    errMsg: error.message,
-                    isLoading: false
-                })) // setCurrentLocation end 
-            } // error end
-        ) // getCurrentPosition end
-        // initialMap이 생성되지 않았다면 실행 X
-        if (!initialMap) return;
-        // initialMap이 생성되었다면, 초기 위치 기준으로 bounds 얻기
-        const bounds = initialMap.getBounds();
-        const southWest = bounds.getSouthWest();
-        const northEast = bounds.getNorthEast();
-        // 얻은 bounds를 통해 bounds 업데이트하기
-        setBounds({
-            south : southWest.getLat(),
-            west : southWest.getLng(),
-            north : northEast.getLat(),
-            east : northEast.getLng()
-        });
+                (error) => {
+                    setCurrentLocation((prev) => ({
+                        ...prev,
+                        errMsg: error.message,
+                        isLoading: false
+                    })) // setCurrentLocation end 
+                } // error end
+            ) // getCurrentPosition end
+            // initialMap이 생성되지 않았다면 실행 X
+            if (!initialMap) return;
+            // initialMap이 생성되었다면, 초기 위치 기준으로 bounds 얻기
+            const bounds = initialMap.getBounds();
+            const southWest = bounds.getSouthWest();
+            const northEast = bounds.getNorthEast();
+            // 얻은 bounds를 통해 bounds 업데이트하기
+            setBounds({
+                south: southWest.getLat(),
+                west: southWest.getLng(),
+                north: northEast.getLat(),
+                east: northEast.getLng()
+            });
         } else {    // geolocation을 통해 위도·경도를 얻을 수 없다면
             setCurrentLocation((prev) => ({
                 ...prev,
@@ -66,47 +66,70 @@ export default function KakaoMap(props){
         } // if end
     }, [initialMap]); // useEffect end
     // =================== bounds Axios POST ===================
+    const [markers, setMarkers] = useState(null);
     const postBounds = async () => {
         try {
-            const option = {withCredentials: true};
+            const option = { withCredentials: true };
             const response = await axios.get(`http://localhost:8080/markersgps/getbycurrentlatlng?south=${bounds.south}&north=${bounds.north}&west=${bounds.west}&east=${bounds.east}`, option);
             const data = response.data;
-            console.log(data);
+            setMarkers(data);
         } catch (error) {
             console.log(error);
         } // try-catch end
     } // func end
-
+    // =================== useEffect - [] ===================
+    useEffect(() => {
+        postBounds();
+        console.log(markers);
+    }, [bounds])
     // =================== return ===================
-    return(
+    return (
         <>
-        <Map
-            center={
-                currentLocation.center
-            }
-            style={{
-                width: '1000px',
-                height: '600px'
-            }}
-            level={3}
-            onBoundsChanged={(map) => {
-                const bounds = map.getBounds();
-                const southWest = bounds.getSouthWest();
-                const northEast = bounds.getNorthEast();
-                setBounds({
-                    south : southWest.getLat(),
-                    west : southWest.getLng(),
-                    north : northEast.getLat(),
-                    east : northEast.getLng()
-                });
-            }}
-            onCreate={setInitialMap}
-        />
-        <h3>북쪽 : {bounds.north}</h3>
-        <h3>남쪽 : {bounds.south}</h3>
-        <h3>동쪽 : {bounds.east}</h3>
-        <h3>서쪽 : {bounds.west}</h3>
-        <button onClick={postBounds}>버튼</button>
+            <Map
+                center={
+                    currentLocation.center
+                }
+                style={{
+                    width: '1000px',
+                    height: '600px'
+                }}
+                level={3}
+                onTileLoaded={(map) => {
+                    const bounds = map.getBounds();
+                    const southWest = bounds.getSouthWest();
+                    const northEast = bounds.getNorthEast();
+                    setBounds({
+                        south: southWest.getLat(),
+                        west: southWest.getLng(),
+                        north: northEast.getLat(),
+                        east: northEast.getLng()
+                    });
+                }}
+                onCreate={setInitialMap}
+            >
+                {markers && markers.map((marker, index) => (
+                    <MapMarker
+                        key={marker.pno}
+                        position={{
+                            lat: marker.mapy,
+                            lng: marker.mapx
+                        }}
+                        image={{
+                            src: xxx,
+                            size: {
+                                width: 80,
+                                height: 80
+                            }
+                        }}
+                        title={'나중에 변경'}
+                    />
+                ))}
+            </Map>
+            <h3>북쪽 : {bounds.north}</h3>
+            <h3>남쪽 : {bounds.south}</h3>
+            <h3>동쪽 : {bounds.east}</h3>
+            <h3>서쪽 : {bounds.west}</h3>
+            <button onClick={postBounds}>버튼</button>
         </>
     ) // return end
 } // func end
