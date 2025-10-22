0c95c57c9f718d95f4a5f071cd996c1d719a65f3
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-10-22 15:37:42 +0900

    docs : 샘플데이터 수정 - 관리자 및 사이트 정보에서 지자체 샘플 데이터 삭제

--- src/main/resources/sql/data_ktour_headquarter.sql
+++ src/main/resources/sql/data_ktour_headquarter.sql
@@ -4,9 +4,7 @@ INSERT INTO k_tour_headquarter.SiteInfo
     (siName, siDomain, siIntro, siLogo, siFavicon, siMarker, siTel, siPrivacyOfficer, siEmail, siKeywords, siIsPublic)
 VALUES
 	('본사 사이트', 'ktour.kr', '본사의 메인 사이트입니다.', 'headquarterLogo.png', 'headquarterFavicon.png',
-     'headquarterMarker.jpg', '032-521-8889', '김진숙', 'ktour@tjoeun.com', 'ktour, 여행지도, SaaS', 1),
-	('고성군 놀러가자!', 'goseong.rootlab.kr', 'DMZ와 맞닿은 침복단의 청정 자연, 에메랄드빛 동해 바다와 금강산의 수려한 산세가 어우러진 평화와 힐링의 땅입니다.<이하 생략···>', 'goseongLogo.png', 'goseongFavicon.png',
-     'goseongMarker.jpg', '033-680-3114', '이상근', 'goseong@tjoeun.com', '해수욕장, 전망대, 강원도, 동해, 바다, 여행, 명소', 1);
+      'headquarterMarker.jpg', '032-521-8889', '김진숙', 'ktour@tjoeun.com', 'ktour, 여행지도, SaaS', 1);
 
 -- ------------------------------------ 관리자정보(자체 테이블) -------------------------------------------
 INSERT INTO k_tour_headquarter.manager 
@@ -15,10 +13,7 @@ INSERT INTO k_tour_headquarter.manager
 VALUES
 -- 1. 시스템 관리자
 	(1,'admin','admin', '김진숙', 'admin', '여', '032-111-2222', 'root.kjs82@gmail.com',
-     '인천광역시 부평구 경원대로 1368', NULL, TRUE, TRUE, TRUE, '본사(시스템관리자)입니다.', 1),
--- 2. 지자체 관리자
-	(2, 'goseong', 'goseong', '고길동', 'goseong', '남', '033-880-3114', 'goseong@tjeoun.com',
-     '강원도 고성군 간성읍 간성중앙길 7', NULL, TRUE, TRUE, TRUE, '251014 고성관리자 입니다.', 2);
+     '인천광역시 부평구 경원대로 1368', NULL, TRUE, TRUE, TRUE, '본사(시스템관리자)입니다.', 1);
 
 -- ------------------------------------ 콘텐츠타입( #TourAPI 연동테이블 ) -------------------------------------------
 INSERT INTO k_tour_headquarter.contentType 
