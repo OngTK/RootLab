e5ab3ee86182847abb03ef9dcfbe3920befd6021
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-10-22 22:10:52 +0900

    refactor : 테스트 KakaoMap.jsx 코드 정리 - 가독성을 위한 코드 정리와 useEffect 의존성 분리

--- src/main/ktour/src/kakaomap/KakaoMap.jsx
+++ src/main/ktour/src/kakaomap/KakaoMap.jsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import xxx from '../assets/contentTypeMarker/festival.png'
 export default function KakaoMap(props) {
     UseKakaoLoader();
+    // =================== useState 선언부 ===================
+    const [markers, setMarkers] = useState(null);
     // =================== 렌더링 기준 사방좌표 구하기 ===================
     const [bounds, setBounds] = useState({
         south: "0.0",
@@ -22,7 +24,7 @@ export default function KakaoMap(props) {
         errMsg: null,
         isLoading: true
     }); // useState end
-    // =================== useEffect - [initialMap] ===================
+    // =================== useEffect - [] : 컴포넌트 마운트될 때, 최초 1번 실행 ===================
     useEffect(() => {
         // geolocation을 통해 위도·경도를 얻을 수 있다면
         if (navigator.geolocation) {
@@ -34,29 +36,15 @@ export default function KakaoMap(props) {
                         lng: location.coords.longitude  // 경도
                     },
                     isLoading: false
-                })) // setCurrentLocation end
-            }, // location end
-                (error) => {
-                    setCurrentLocation((prev) => ({
-                        ...prev,
-                        errMsg: error.message,
-                        isLoading: false
-                    })) // setCurrentLocation end 
-                } // error end
+                })); // setCurrentLocation end
+            }, (error) => {
+                setCurrentLocation((prev) => ({
+                    ...prev,
+                    errMsg: error.message,
+                    isLoading: false
+                })); // setCurrentLocation end 
+            } // error end
             ) // getCurrentPosition end
-            // initialMap이 생성되지 않았다면 실행 X
-            if (!initialMap) return;
-            // initialMap이 생성되었다면, 초기 위치 기준으로 bounds 얻기
-            const bounds = initialMap.getBounds();
-            const southWest = bounds.getSouthWest();
-            const northEast = bounds.getNorthEast();
-            // 얻은 bounds를 통해 bounds 업데이트하기
-            setBounds({
-                south: southWest.getLat(),
-                west: southWest.getLng(),
-                north: northEast.getLat(),
-                east: northEast.getLng()
-            });
         } else {    // geolocation을 통해 위도·경도를 얻을 수 없다면
             setCurrentLocation((prev) => ({
                 ...prev,
@@ -64,10 +52,11 @@ export default function KakaoMap(props) {
                 isLoading: false
             })) // setCurrentLocation end
         } // if end
-    }, [initialMap]); // useEffect end
-    // =================== bounds Axios POST ===================
-    const [markers, setMarkers] = useState(null);
-    const postBounds = async () => {
+    }, []) // useEffect end
+    // =================== bounds Axios GET ===================
+    const getBoundsByAxios = async () => {
+        // bounds가 유효하지 않으면, API 호출 X
+        if (bounds.south === 0.0) return;
         try {
             const option = { withCredentials: true };
             const response = await axios.get(`http://localhost:8080/markersgps/getbycurrentlatlng?south=${bounds.south}&north=${bounds.north}&west=${bounds.west}&east=${bounds.east}`, option);
@@ -77,12 +66,15 @@ export default function KakaoMap(props) {
             console.log(error);
         } // try-catch end
     } // func end
-    // =================== useEffect - [] ===================
+    // =================== useEffect - [bounds] ===================
     useEffect(() => {
-        postBounds();
+        getBoundsByAxios();
         console.log(markers);
     }, [bounds])
     // =================== return ===================
+    if (currentLocation.isLoading) {
+        return <div>현재 위치를 불러오는 중입니다...</div>;
+    } // if end
     return (
         <>
             <Map
@@ -94,7 +86,7 @@ export default function KakaoMap(props) {
                     height: '600px'
                 }}
                 level={3}
-                onTileLoaded={(map) => {
+                onIdle={(map) => {
                     const bounds = map.getBounds();
                     const southWest = bounds.getSouthWest();
                     const northEast = bounds.getNorthEast();
@@ -129,7 +121,7 @@ export default function KakaoMap(props) {
             <h3>남쪽 : {bounds.south}</h3>
             <h3>동쪽 : {bounds.east}</h3>
             <h3>서쪽 : {bounds.west}</h3>
-            <button onClick={postBounds}>버튼</button>
+            <button onClick={getBoundsByAxios}>버튼</button>
         </>
     ) // return end
 } // func end
