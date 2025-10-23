981698e7099fd0c1b8747fbac56dac87fd1cda72
root-kjs <root.kjs82@gmail.com>
2025-10-23 18:11:22 +0900

    feat(layout): LNB 작업중 - 사용자단 MUI는 안정훈씨와 협의하여 정훈씨가 나중에 작업하기로 함.

--- src/main/ktour/src/App.jsx
+++ src/main/ktour/src/App.jsx
@@ -7,11 +7,11 @@
  */
 import { BrowserRouter, Route, Routes } from "react-router-dom";    // 라우터(URL 경로) 
 import { lazy, Suspense } from "react";                             // 코드 스플리팅(필요한 시점 비동기 로딩_Lazy Loading)
-import "@assets/admin/css/reset.css";                               // 전역 리셋 CSS적용
+import "@assets/reset.css";                                         // 전역 리셋 CSS적용
 
-const UserRouter = lazy(() => import("@user/UserRouter"));          // 사용자단 라우터
+const UserRouter  = lazy(() => import("@user/UserRouter"));         // 사용자단 라우터
 const AdminRouter = lazy(() => import("@admin/AdminRouter"));       // 관리자단 라우터
-const KakaoMap = lazy(() => import("./kakaomap/KakaoMap"));         // 카카오맵 API 테스트용 페이지
+const KakaoMap    = lazy(() => import("./kakaomap/KakaoMap"));      // 카카오맵 API 테스트용 페이지
 
 export default function App() {
     return (
