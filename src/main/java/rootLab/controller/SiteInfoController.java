02f2e3bc6afd688ab3e10b313d8033cfc71806ec
root-kjs <root.kjs82@gmail.com>
2025-10-30 11:40:47 +0900

    chore: 사용자단(메인)팝업배너리턴오타 제거 - 지도 스타일 조정 - SQL 김진숙 테스트 수정

--- src/main/java/rootLab/controller/SiteInfoController.java
+++ src/main/java/rootLab/controller/SiteInfoController.java
@@ -48,9 +48,9 @@ public class SiteInfoController {
         if (siIsPublic != null) siteInfoCriteria.setSiIsPublic(siIsPublic);
         if (siDomain != null) siteInfoCriteria.setSiDomain(siDomain);
         if (siName != null) siteInfoCriteria.setSiName(siName);
-        siteInfoCriteria.setPage(page);
-        siteInfoCriteria.setPageSize(pageSize);
-        siteInfoCriteria.setStartRow((page - 1) * pageSize);
+        // siteInfoCriteria.setPage(page);
+        // siteInfoCriteria.setPageSize(pageSize);
+        // siteInfoCriteria.setStartRow((page - 1) * pageSize);
         return ResponseEntity.ok(siteInfoService.searchSites(siteInfoCriteria));
     } // func end
 
