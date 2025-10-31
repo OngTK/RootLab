fee3c51b98bbc3589799336c28d237bc8da50089
juju9595 <git>
2025-10-31 09:27:11 +0900

    docs(react) : firebase.js 식별키 수정

--- src/main/ktour/src/firebase/firebase.js
+++ src/main/ktour/src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getMessaging, getToken, onMessage, isSupported } from "firebase/messagi
 
 // ✅ Firebase 설정값
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+   apiKey: "AIzaSyA-zW5DeMa_Q_BtEEJpzdUTkIRHwqbCM1E",
+  authDomain: "k-tour-team-projedct.firebaseapp.com",
+  projectId: "k-tour-team-projedct",
+  storageBucket: "k-tour-team-projedct.firebasestorage.app",
+  messagingSenderId: "560943551271",
+  appId: "1:560943551271:web:7b2f5769c05b0a211b8f02",
+  measurementId: "G-XSF0V65730"
 };
 
 // ✅ Firebase 앱 초기화
